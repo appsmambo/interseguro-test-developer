19a20ff53d27346d2049b368cfd47ca77976ba0e
Juan Quintanilla <quintanilla.peru@gmail.com>
2021-04-22 06:45:00 -0500

    Iniciar controller y route.

---
+++ app/Http/Controllers/MatrizController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatrizController extends Controller
+{
+    public function postRotar(Request $request)
+    {
+        echo 'postRotar';
+    }
+}
